54117da12f30130ef13e75fdd0f919358a479c57
ann <anna@synqq.com>
2018-04-12 00:19:32 +0500

    Add switcher for smooth and not smooth animations

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,5 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  Component
+} from '@angular/core';
+import * as $ from 'jquery';
 
 
 @Component({
@@ -8,4 +13,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent { }
+export class HeaderComponent {
+
+  activateSmoothTransitions(e) {
+   $('.with-transition').removeClass('transitions-disabled');
+   alert('Transitions were applied');
+  }
+  deactivateSmoothTransitions(e) {
+    $('.with-transition').addClass('transitions-disabled');
+    alert('Transitions were removed');
+  }
+}
